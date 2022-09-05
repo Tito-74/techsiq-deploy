a2db21eb0fbb4ea9889887e77de8f22ca4f7efb8
Tito-74 <kipkirui133@gmail.com>
2022-09-05 20:34:14 +0300

    modify blog model

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,8 @@ class Category(models.Model):
 
 class Blog(models.Model):
   title = models.CharField(max_length=100, null=False, blank=False)
-  body = models.TextField(max_length=350, null=False, blank=False)
+  body = models.TextField(blank=False, null=False)
+  # ( null=False, blank=False)
   image = CloudinaryField('images')
   date_published = models.DateTimeField(auto_now_add=True,verbose_name='date_published')
   Author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
