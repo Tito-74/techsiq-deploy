c75bf3819e6692777df0681c8afa33887840f51f
Tito-74 <kipkirui133@gmail.com>
2022-09-14 21:05:37 +0300

    added richtext edditor

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
-from cloudinary.models import CloudinaryField
+# from cloudinary.models import CloudinaryField
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -14,9 +15,10 @@ class Category(models.Model):
 
 class Blog(models.Model):
   title = models.CharField(max_length=100, null=False, blank=False)
-  body = models.TextField(blank=False, null=False)
+  # body = models.TextField(blank=False, null=False)
   # ( null=False, blank=False)
-  image = CloudinaryField('images')
+  # image = CloudinaryField('images')
+  body = RichTextField(null=False, blank=False)
   date_published = models.DateTimeField(auto_now_add=True,verbose_name='date_published')
   Author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
@@ -29,8 +31,9 @@ class TechsiqTeam(models.Model):
   name = models.CharField(max_length=255)
   Title = models.CharField(max_length=100)
   social_media_link = models.URLField(max_length=255)
-  detailsInfo = models.TextField(max_length=500)
-  image = CloudinaryField('images')
+  # detailsInfo = models.TextField(max_length=500)
+  # image = CloudinaryField('images')
+  detailsInfo = RichTextField(null=False, blank=False)
 
 
   def __str__(self):
