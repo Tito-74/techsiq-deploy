8d8551714034cf2e6483c6e1c7951f3e19f2dcd6
Tito-74 <kipkirui133@gmail.com>
2022-09-17 23:59:12 +0300

    added image on the model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
-# from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField
 from ckeditor.fields import RichTextField
 
 
@@ -17,7 +17,7 @@ class Blog(models.Model):
   title = models.CharField(max_length=100, null=False, blank=False)
   # body = models.TextField(blank=False, null=False)
   # ( null=False, blank=False)
-  # image = CloudinaryField('images')
+  image = CloudinaryField('images')
   body = RichTextField(null=False, blank=False)
   date_published = models.DateTimeField(auto_now_add=True,verbose_name='date_published')
   Author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
